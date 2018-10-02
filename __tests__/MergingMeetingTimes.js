4e2470203677dfa57a77492b998e145ee01cbf21
Henry Han <potatoeater0127@gmail.com>
2018-10-01 20:28:59 -0700

    Add some more test cases for Merging Meeting Times

---
+++ __tests__/MergingMeetingTimes.js
@@ -0,0 +1,44 @@
+const { mergeRanges } = require('../MergingMeetingTimes');
+
+const meetings1 = [
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 5 },
+  { startTime: 4, endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9, endTime: 10 },
+];
+const merged1 = [
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 8 },
+  { startTime: 9, endTime: 12 },
+];
+
+const meetings2 = [
+  { startTime: 10, endTime: 12 },
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 5 },
+];
+const merged2 = [
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 5 },
+  { startTime: 10, endTime: 12 },
+];
+
+const meetings3 = [
+  { startTime: 0, endTime: 12 },
+  { startTime: 0, endTime: 1 },
+];
+const merged3 = [
+  { startTime: 0, endTime: 12 },
+];
+test('should merge correctly', () => {
+  expect(mergeRanges(meetings1)).toEqual(merged1);
+});
+
+test('should have the same meetings when no intersections', () => {
+  expect(mergeRanges(meetings2)).toEqual(merged2);
+});
+
+test('should return the longer one when one is entirely contained by the other', () => {
+  expect(mergeRanges(meetings3)).toEqual(merged3);
+});
